7d900d79192657517f1eaa1aec77a40377f13bbd
TheSanvi <sanvivermavns@gmail.com>
2025-02-11 22:18:08 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -33,7 +33,7 @@ class _SplashScreenState extends State<SplashScreen>
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => UserDetailsScreen()),
+        MaterialPageRoute(builder: (context) => HomeScreen()),
       );
     });
   }
